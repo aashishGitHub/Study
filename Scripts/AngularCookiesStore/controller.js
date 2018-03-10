ec1db2be8202154162cf1bb5af719c255f6262e3
Aashish Kumar <aashish123pqr@gmail.com>
2018-03-10 16:56:21 +0530

    test

---
+++ Scripts/AngularCookiesStore/controller.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controller', controller);
+
+    controller.$inject = ['$scope', 'factory','$q'];
+
+    function controller($scope, factory, $q) {
+        $scope.title = 'controller'; 
+        var deferred = $q.defer();
+        $q.all([factory.getCookies()])
+            .then(function (response) {
+                $scope.cookies = response[0];
+            })
+       
+        activate();
+
+        function activate() { }
+    }
+})();
